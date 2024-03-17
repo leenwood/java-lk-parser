ad0b48a76b56293e46dd33b438c4da09f4bd102e
leenwood <george200135@gmail.com>
2024-03-17 14:51:33 +0500

    Добавил корс политику

---
+++ src/main/java/com/parser/lk/controller/WebConfig.java
@@ -0,0 +1,18 @@
+package com.parser.lk.controller;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/v1/**")
+                .allowedOrigins("http://45.90.35.14")
+                .allowedMethods("GET", "POST", "PATCH", "PUT", "OPTION");
+    }
+}
